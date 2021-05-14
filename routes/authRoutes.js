ea44a87b9c58bf30c13c8f415948b90973ec0d4e
DarkFlamingo <mgavatuk@gmail.com>
2021-05-14 13:37:20 +0300

    Add login endpoint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,15 +4,24 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 
 const router = Router();
 
-router.post('/login', (req, res, next) => {
+router.post(
+  '/login',
+  (req, res, next) => {
     try {
-        // TODO: Implement login action (get the user if it exist with entered credentials)
-        res.data = data;
+      // TODO: Implement login action (get the user if it exist with entered credentials)
+      const data = req.body;
+      const user = AuthService.login(data);
+      if (!user) {
+        throw { msg: 'User not found', status: 400 };
+      }
+      res.data = user;
     } catch (err) {
-        res.err = err;
+      res.err = err;
     } finally {
-        next();
+      next();
     }
-}, responseMiddleware);
+  },
+  responseMiddleware
+);
 
 module.exports = router;
